6e034a0800b9fbbc2e4c7bf550bdabaa8c45ac88
uugncr <ugur.gencer@std.yildiz.edu.tr>
2023-09-25 01:09:58 +0300

    uuu

--- deneme.py
+++ deneme.py
@@ -18,22 +18,16 @@ t1 = np.linspace(50, 200, M)
 
 y = gauss(t, 100, 80, 50) 
 y_data = y + np.random.uniform(0, 50, N)
-#popt, pcov = curve_fit(gauss, t, y_data) 
+popt, pcov = curve_fit(gauss, t, y_data) 
 #print(popt)
 
 y1 = expo(t1, 100,100)
 y1_data = y1 + np.random.uniform(50, 200, M)
-#popt, pcov = curve_fit(expo, t1, y1_data) 
+popt, pcov = curve_fit(expo, t1, y1_data) 
 #print(popt)
+plt.plot(t, y, '-b')
+plt.plot(t1, y1, '-r')
 
 plt.plot(t, y_data, '.')
 plt.plot(t1, y1_data, '*')
-
-
-plt.plot(t, gauss(t, 200, 160, 183), label='Gaussian')
-plt.plot(t1, expo(t1, 200, 100), label='Exponential')
-plt.plot(t1, y, label='Mixed')
-plt.legend()
-plt.xlabel('t')
-plt.ylabel('y')
 plt.show()
--- opt.py
+++ opt.py
@@ -7,18 +7,22 @@ def profile(f, f0, gamma, offset):  #frekansa bagli bir egriyi gosteren Loorentz
     return 1 / ((f - f0) / gamma)**2 * offset # merkezi = f0, genisligi = gamma, 0'dan yuksekligi = offset
 
 N = 500
-f = np.linspace(-10, 10, N) #fonksiyon araligim
-y = profile(f, 1, 0.5, 0.5) #degerler
+f = np.linspace(0, 10, N) #fonksiyon araligim
+y = profile(f, 4, 1, 1) #degerler
 
-y_data = y * np.random.uniform(0.3, 0.3, N) #"y" mukkemmel data'nin ustune random gurultu ekledik.
+#plt.plot(f, y)
 
-popt, pcov = curve_fit(profile, f, y_data, p0 = [0.5 ,1, -0.1 ]) #1. degerim fonksiyon, 2. degerim aralik, 3. ddegerim fit ederken baslangic sartlarini vererek islemi hizlandirmak 
-#print(popt) # popt ise ooptimizasyonun sonucunda profilin parametlerini verir 
+y_data = y + np.random.uniform(3, 5, N) #"y" mukkemmel data'nin ustune random gurultu ekledik.
+#plt.plot(f, y_data, '.')
+#plt.plot(f, y)
 
-plt.figure(figsize = (10,8))
+popt, pcov = curve_fit(profile, f, y_data, p0 = [4 ,1, 4]) #1. degerim fonksiyon, 2. degerim aralik, 3. ddegerim fit ederken baslangic sartlarini vererek islemi hizlandirmak 
+print(popt) # popt ise ooptimizasyonun sonucunda profilin parametlerini verir 
+
+
+plt.figure(figsize = (10,7))
 plt.plot(f, y_data, 'k-')
 plt.plot(f, profile(f, *popt), '-r')
-plt.plot(f, profile(f, 1, 0.5, 0.5), '-b')
+plt.plot(f, profile(f, 4, 1, 4), '-b')
 
-plt.plot(f, y_data, '.')
 plt.show()
