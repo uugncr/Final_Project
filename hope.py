37fbb672f8fce4ce4d6133eee2f51de3cc11bbe3
uugncr <ugur.gencer@std.yildiz.edu.tr>
2023-10-03 22:44:03 +0300

    new

--- hope.py
+++ hope.py
@@ -11,18 +11,21 @@ def gauss(t, t0, s_r, s_f):
         return mix
 
 t = np.linspace(0, 1024)
-t0 = 500
+t0 = np.random.uniform(0,1024)
 s_r = 100
-s_f = 300
+s_f = 400
 y = np.array([gauss(i, t0, s_r, s_f) for i in t])
-
+print(y)
 
 ###################################################
+"""
 perfect_gauss = np.exp(-(t - t0) ** 2 / (2 * s_r ** 2)) / (s_r * np.sqrt(2 * np.pi))
 plt.plot(t, perfect_gauss, '-b')
+"""
 ###################################################
 
-plt.plot(t, y,'-r')
+
+plt.plot(t, y,'-r' , marker = '.')
 plt.xlabel('t')
 plt.ylabel('gauss(t)')
 
