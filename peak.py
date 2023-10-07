e2b68c9942313e9b268ae73bdaa4367303ab2e96
uugncr <ugur.gencer@std.yildiz.edu.tr>
2023-10-07 17:05:44 +0300

    new

--- peak.py
+++ peak.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline
 
-def gauss(t, t0, s_r, s_f):
+def f(t, t0, s_r, s_f):
     N = 20
     if t < t0:
         expo = N * np.exp(-(t - t0)**2 / (2 * (s_r ** 2)))
@@ -11,25 +12,66 @@ def gauss(t, t0, s_r, s_f):
         return mix
 
 
-t = np.linspace(0, 1024,1024)
-t0 = np.random.uniform(0,1024)
-
+t = np.linspace(0, 1024,2048)
+#t0 = np.random.uniform(0,1023)
+#print(t0)
+t0 = 512
 s_r = 6
 s_f = 25
 
+y = np.array([f(i, t0, s_r, s_f) for i in t])
+#print(y)
+np.savetxt('gaussian.csv', y, delimiter=',')
 
-y = np.array([gauss(i, t0, s_r, s_f) for i in t])
+cs = CubicSpline(t,y)
+t_interp = t 
+y_interp = cs(t_interp)
 
-x = t
-y = y
+desired_value = 2
+tolerance = 1e-0
 
-print(x)
-print(y)
+for i in range(len(y_interp) - 1):
+    if abs(y_interp[i] - desired_value) <= tolerance and abs(y_interp[i + 1] - desired_value) <= tolerance:
+        print(f"Fonksiyon 2 olduğu nokta: x={t_interp[i]}, y={y_interp[i]}")
 """
-plt.plot(t, y,'-b' , marker = '.')
+def find_t(y, t, t0):
+    closest_t = None
+    min_diff = float('inf')
+
+    for i in range(len(t)):
+        if abs(y[i] - (t0 * 0.01)) < min_diff:
+            min_diff = abs(y[i] - (t0 * 0.1))
+            closest_t = t[i]
+
+    return closest_t
+
+# Call the function to find the closest t value to y = t0 * 0.1
+closest_t = find_t(y, t, t0)
+print(closest_t)
+
+#formatted_array = [f"{num:.10f}" for num in y]
+#print(formatted_array)
+"""
+
+"""
+t1=[]
+
+desir_y1 =20
+if gauss(t, t0, s_r, s_f) == desir_y1 * 0.1:
+    t1.append()
+
+print(t1)"""
+
+"""
+index = np.argmax(y)
+t0_value = t[index]
+y_value = y[index]
+print(t0_value, y_value)
+
+"""
+plt.plot(t, y, marker = '.')
 plt.xlabel('t')
 plt.ylabel('V')
 
 plt.show()
 
-"""
--- time_range.py
+++ time_range.py
@@ -1,13 +1,47 @@
-import peak as p
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import interpolate
+from scipy.interpolate import CubicSpline
 
-index = np.argmax(p.y)
-t0_value = p.t[index]
-y_value = p.y[index]
+def f(t, t0, s_r, s_f):
+    N = 20
+    if t < t0:
+        expo = N * np.exp(-(t - t0)**2 / (2 * (s_r ** 2)))
+        return expo
+    else:
+        mix = N * np.exp(- (t - t0)**2 /s_f **2 )
+        return mix
 
-print(t0_value, y_value)
 
-t0_inter = interpolate.interp1d(p.y)
-t0_new = [p.t0+1, p.t0-1]
+t = np.linspace(0, 1024,1024)
+#t0 = np.random.uniform(0,1023)
+#print(t0)
+t0 = 512
+s_r = 6
+s_f = 25
+
+y = np.array([f(i, t0, s_r, s_f) for i in t])
+#print(y)
+np.savetxt('gaussian_2.csv', y, delimiter=',')
+
+cs = CubicSpline(t,y)
+t_interp = np.linspace(0, 1024, 10000)
+y_interp = cs(t_interp)
+
+desired_value = 2
+tolerance = 1e-2
+
+for i in range(len(y_interp) - 1):
+    if abs(y_interp[i] - desired_value) <= tolerance and abs(y_interp[i + 1] - desired_value) <= tolerance:
+        print(f"Fonksiyon 2 olduğu nokta: x={t_interp[i]}, y={y_interp[i]}")
+
+
+
+
+
+
+plt.plot(t, y, marker = '.')
+plt.xlabel('t')
+plt.ylabel('V')
+
+plt.show()
+
