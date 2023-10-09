e8e8be7f75b8dfb818d55a9cb9e7a2dbc0842786
uugncr <ugur.gencer@std.yildiz.edu.tr>
2023-10-09 21:47:22 +0300

    new

--- time_range.py
+++ time_range.py
@@ -26,36 +26,36 @@ np.savetxt('gaussian_2.csv', y, delimiter=',')
 
 t1, t2, t3, t4 = None, None, None, None
 cs = CubicSpline(t, y)
-t_interp = np.linspace(0, 1024, 10000)
+t_interp = np.linspace(0, 1024, 100000)
 y_interp = cs(t_interp)
 
 
 desired_value = N * 0.1
 tolerance = 1e-1
 
-desired_value1 = N * 0.1
+desired_value_1 = N * 0.1
 for i in range(len(y_interp) - 1):
-    if abs(y_interp[i] - desired_value1) <= tolerance and abs(y_interp[i + 1] - desired_value1) <= tolerance:
+    if abs(y_interp[i] - desired_value_1) <= tolerance and abs(y_interp[i + 1] - desired_value_1) <= tolerance:
         t1 = t_interp[i]
         break
 
-desired_value2 = N * 0.9
+desired_value_2 = N * 0.9
 for i in range(len(y_interp) - 1):
-    if abs(y_interp[i] - desired_value2) <= tolerance and abs(y_interp[i + 1] - desired_value2) <= tolerance:
+    if abs(y_interp[i] - desired_value_2) <= tolerance and abs(y_interp[i + 1] - desired_value_2) <= tolerance:
         t2 = t_interp[i]
         break
 
 
-desired_value3 = N * 0.9
+desired_value_3 = N * 0.9
 for i in range(len(y_interp) - 1, 0, -1):
-    if abs(y_interp[i] - desired_value3) <= tolerance and abs(y_interp[i - 1] - desired_value3) <= tolerance:
+    if abs(y_interp[i] - desired_value_3) <= tolerance and abs(y_interp[i - 1] - desired_value_3) <= tolerance:
         t3 = t_interp[i]
         break
 
 
-desired_value4 = N * 0.1
+desired_value_4 = N * 0.1
 for i in range(len(y_interp) - 1, 0, -1):
-    if abs(y_interp[i] - desired_value4) <= tolerance and abs(y_interp[i - 1] - desired_value4) <= tolerance:
+    if abs(y_interp[i] - desired_value_4) <= tolerance and abs(y_interp[i - 1] - desired_value_4) <= tolerance:
         t4 = t_interp[i]
         break
 
@@ -65,9 +65,10 @@ print(f"t1: {t1}, t2: {t2}, t0:{t0},  t3: {t3}, t4: {t4}")
 integral, error = integrate.quad(lambda x: cs(x), t0, t4)
 integral2, error2 = integrate.quad(lambda x: cs(x), t1, t4)
 
-oran = integral/integral2
-print(oran)
-
+print("integral_oranlari :", integral/integral2)
+delta_rise = t2 - t1
+delta_fall = t4 - t3
+print("delta_rise :", delta_rise, "delta_fall :", delta_fall)
 
 plt.plot(t, y, marker='.')
 plt.xlabel('t')
