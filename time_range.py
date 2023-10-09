848d507e9492a570175f7a3850603356d9655aaf
uugncr <ugur.gencer@std.yildiz.edu.tr>
2023-10-09 23:56:41 +0300

    new

--- time_range.py
+++ time_range.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import CubicSpline
 from scipy import integrate
 
+#Fonksiyon 
+
 def f(t, t0, s_r, s_f,N):
     if t < t0:
         expo = N * np.exp(-(t - t0)**2 / (2 * (s_r ** 2)))
@@ -11,10 +13,9 @@ def f(t, t0, s_r, s_f,N):
         mix = N * np.exp(- (t - t0)**2 /s_f **2 )
         return mix
 
-
 t = np.linspace(0, 1024, 1024)
-N = 20
 t0 = np.random.uniform(0, 1024)
+N = 20
 s_r = 6
 s_f = 25
 
@@ -25,6 +26,7 @@ np.savetxt('gaussian_2.csv', y, delimiter=',')
 #Interplasyon 
 
 t1, t2, t3, t4 = None, None, None, None
+
 cs = CubicSpline(t, y)
 t_interp = np.linspace(0, 1024, 100000)
 y_interp = cs(t_interp)
@@ -59,28 +61,25 @@ for i in range(len(y_interp) - 1, 0, -1):
         t4 = t_interp[i]
         break
 
-print(f"t1: {t1}, t2: {t2}, t0:{t0},  t3: {t3}, t4: {t4}")
+print(f"t1: {t1}\nt2: {t2}\nt0: {t0}\nt3: {t3}\nt4: {t4}")
 
 #Integral 
 
 integral3, error = integrate.quad(lambda x: cs(x), t0, t4)
 integral4, error2 = integrate.quad(lambda x: cs(x), t1, t4)
-print("integral_oranlari_cs :", integral3/integral4)
+print("integral_oranlari_cs:", integral3/integral4)
 
 
 f_lambda = lambda x: f(x, t0, s_r, s_f, N)
 integral, error = integrate.quad(f_lambda, t0, t4)
 integral2, error2 = integrate.quad(f_lambda, t1, t4)
-print("integral_oranlari_duz :", integral/integral2)
-
-
-
-
-
+print("integral_t0_t4:",integral)
+print("integral_t1_t4:",integral2)
+print("integral_oranlari_duz:", integral/integral2)
 
 delta_rise = t2 - t1
 delta_fall = t4 - t3
-print("delta_rise :", delta_rise, "delta_fall :", delta_fall)
+print("delta_rise:", delta_rise, "\ndelta_fall:", delta_fall)
 
 plt.plot(t, y, marker='.')
 plt.xlabel('t')
