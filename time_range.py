e54d11e2279f1d11e61b2a51e852a8e683cfb4b4
uugncr <ugur.gencer@std.yildiz.edu.tr>
2023-10-22 20:16:50 +0300

    uuu

--- time_range.py
+++ time_range.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy import integrate
 from scipy import interpolate
 
-#Fonksiyon aBB
+#Fonksiyon
 
 def f(t, t0, s_r, s_f,N):
     if t < t0:
@@ -14,16 +14,21 @@ def f(t, t0, s_r, s_f,N):
         return mix
 
 t = np.linspace(0, 1024, 1024)
-
 t0 = np.random.uniform(0, 1024)
 N = np.random.uniform(20, 500)
 s_r = np.random.uniform(5.5, 6.5)
-s_f = np.random.uniform(48, 52)
+s_f = np.random.uniform(23, 102)
 
+print("N:", N, "\nN%10:", N * 0.1, "\nN%90:", N * 0.9, "\ns_r:", s_r, "\ns_f:", s_f)
+
+########
 y = np.array([f(i, t0, s_r, s_f, N) for i in t])
-
+noise = np.random.uniform(4, 12, 1024)
+peak = noise + y
+#np.savetxt('noise.csv', noise, delimiter=',')
 #np.savetxt('gaussian_2.csv', y, delimiter=',')
-print("N:", N, "\nN%10:", N * 0.1, "\nN%90:", N * 0.9)
+########
+# t degerlerinin bulunmasi
 
 t1 = t[np.where(np.logical_and(y < N * 0.1, t < t0))][-1]
 t2 = t[np.where(np.logical_and(y < N * 0.9, t < t0))][-1]
@@ -33,6 +38,7 @@ t4 = t[np.where(np.logical_and(y > N * 0.1, t > t0))][-1]
 print(f"t1: {t1}\nt2: {t2}\nt0: {t0}\nt3: {t3}\nt4: {t4}")
 
 #Integral
+
 f_lambda = lambda x: f(x, t0, s_r, s_f, N)
 integral, error = integrate.quad(f_lambda, t0, t4)
 integral2, error2 = integrate.quad(f_lambda, t1, t4)
@@ -41,12 +47,26 @@ print("integral_t1_t4:",integral2)
 print("integral_oranlari:", integral/integral2)
 
 #Time
+
 delta_rise = t2 - t1
 delta_fall = t4 - t3
+
 print("delta_rise:", delta_rise, "\ndelta_fall:", delta_fall)
 
-plt.plot(t, y, marker='.')
+# % degerlerin cizimi
+
+plt.plot([t[0], t[-1]], [N * 0.1, N * 0.1], 'r--', label='N%10')
+plt.plot([t[0], t[-1]], [N * 0.9, N * 0.9], 'r--', label='N%90')
+
+#noise alt/ust cizimi
+
+plt.plot([t[0], t[-1]], [4, 4], 'g--', label='y = 4')
+plt.plot([t[0], t[-1]], [12, 12], 'g--', label='y = 12')
+
+plt.plot(t, y, 'b--', marker=',')
+plt.plot (t, peak, marker='.')
 plt.xlabel('t')
 plt.ylabel('V')
 
 plt.show()
+
